0fece0fd3660992f95b171d03fbdbbfb159b4ee7
Arnold Daniels <arnold@jasny.net>
2018-10-03 23:15:15 +0200

    Proper PHP examples.
    Default example was ridiculous.
    Second example was actually a phtml template.
    Added short_tag example. Closes #16
    Added stream example. Required in SOLID apps for DI.
    Fixed function approach.
    Turned index.php into default and removed closing tag.
    Fixed OOP example to make sense.

--- php/oop.php
+++ php/oop.php
@@ -1,14 +1,29 @@
 <?php
-class OOP{
+
+declare(strict_types=1);
+
+class StringObject
+{
     private $string;
-    public function setString($string){
+
+    public function __construct(string $string)
+    {
         $this->string = $string;
-        return $this;
     }
-    public function getString(){
+
+    public function append(string $string): self
+    {
+        return new self($this->string . $string);
+    }
+
+    public function __toString()
+    {
         return $this->string;
     }
 }
-$c = new OOP;
-echo $c->setString("Hello World")
-  ->getString();
+
+$hello = new StringObject("Hello");
+$helloWorld = $hello->append(" World");
+
+echo $helloWorld;
+
