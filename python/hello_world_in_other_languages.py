992228d3dd1c5c93c0481d0e72777cff6d79cb10
thegezy <usgezawa@gmail.com>
2019-10-07 15:39:09 +0100

    I added Hello World in two other foreign languages

--- python/hello_world_in_other_languages.py
+++ python/hello_world_in_other_languages.py
@@ -1,2 +1,12 @@
-language = {"en":"hello world", "tr":"merhaba dünya"}
+g = "مرحبا بكم"
+language = {"en":"hello world", "tr":"merhaba dünya", "hau":"Barka da Zuwa!"
+            "ara":g}
 print(language["tr"])
+
+#hello world in HAUSA language
+print(language["hau"])
+
+ #العربية
+#بلغة
+#hello world in ARABIC language
+print(language["ara"])
